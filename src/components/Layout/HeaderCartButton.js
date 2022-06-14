0048e31305829f74aba8d81b1d40fd0e0afbc603
ash14ish <ash14ishh@gmail.com>
2022-06-14 16:02:26 +0530

    food-order-app

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -3,7 +3,7 @@ import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
-const HeaderCartButton = (props) => {
+const HeaderCartButton = props => {
   const cartCtx = useContext(CartContext);
   const [isBtnBumping, setIsBtnBumping] = useState(false);
 
@@ -30,7 +30,7 @@ const HeaderCartButton = (props) => {
       <span className={classes.icon}>
         <CartIcon />
       </span>
-      <span>Your Cart</span>
+      <span className={classes.text}>Your Cart</span>
       <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
